83feac3b81c0189d438ff0a5de026f284b54a3ba
SeaBee <3200104392@zju.edu.cn>
2023-05-11 02:29:40 +0800

    fastjson2 手搓返回值

--- master/src/main/java/main/MasterApplication.java
+++ master/src/main/java/main/MasterApplication.java
@@ -1,5 +1,7 @@
 package main;
 
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +14,7 @@ import java.sql.*;
 @RestController
 @SpringBootApplication
 public class MasterApplication {
+
     @Autowired
     DataSource dataSource;
 
@@ -29,10 +32,12 @@ public class MasterApplication {
         return "hello world";
     }
 
-    // 测试 JDBC
+    // 测试 JDBC（打印，并返回 JSON 数组）
     @RequestMapping("/test")
-    public void testJDBC() {
+    public JSONArray testJDBC() {
         System.out.println(dataSource);
+        // 返回的 JSON 数组
+        JSONArray res = new JSONArray();
         try {
             Connection conn = dataSource.getConnection();
             Statement stmt = conn.createStatement();
@@ -41,15 +46,26 @@ public class MasterApplication {
             // 打印结果
             System.out.println("uid, username, password:");
             while(rs.next()) {
+                // 终端输出
                 System.out.println(rs.getString(2) + ", " + rs.getString(1) + ", " + rs.getString(3));
+                // 搓单个 JSON 对象
+                JSONObject user = new JSONObject();
+                user.put("id", rs.getString(2));
+                user.put("username", rs.getString(1));
+                user.put("password", rs.getString(3));
+                // 塞进数组
+                res.add(user);
             }
             // 释放连接
             rs.close();
             stmt.close();
             conn.close();
+
         } catch (Exception e) {
             System.out.println("数据库连接失败");
             System.out.println(e);
         }
+
+        return res;
     }
 }
